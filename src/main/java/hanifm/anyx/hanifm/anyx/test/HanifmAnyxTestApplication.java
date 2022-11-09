f160509ca1f25c8403102a428c4afe8d906b4d20
Hanif Muhammad <hanif72muhammad@gmail.com>
2022-11-09 09:05:10 +0700

    first init

---
+++ src/main/java/hanifm/anyx/hanifm/anyx/test/HanifmAnyxTestApplication.java
@@ -0,0 +1,13 @@
+package hanifm.anyx.hanifm.anyx.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanifmAnyxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanifmAnyxTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanifm/anyx/hanifm/anyx/test/HanifmAnyxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hanifm.anyx.hanifm.anyx.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanifmAnyxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
